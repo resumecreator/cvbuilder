698960a6e940e9a9601481f7de752fd0a53f0eb8
Sebastian Velo <sebast.velo@gmail.com>
2020-05-24 15:13:15 -0300

    Updates

---
+++ precache-manifest.b3e34c5302689837978594c58674d07d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3b936997364a77d2ef0ff7612387d5",
+    "url": "/cvbuilder/index.html"
+  },
+  {
+    "revision": "f8ebce3259bb06f851d5",
+    "url": "/cvbuilder/static/css/main.b1b9a13a.chunk.css"
+  },
+  {
+    "revision": "09c8fde948269d82ee75",
+    "url": "/cvbuilder/static/js/2.cf4be69e.chunk.js"
+  },
+  {
+    "revision": "c0cd05482c2c076b6ca3754f53f53494",
+    "url": "/cvbuilder/static/js/2.cf4be69e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8ebce3259bb06f851d5",
+    "url": "/cvbuilder/static/js/main.cede9d83.chunk.js"
+  },
+  {
+    "revision": "01f5a49b370ff31e7582",
+    "url": "/cvbuilder/static/js/runtime-main.dab99ccc.js"
+  },
+  {
+    "revision": "58395202fadf1210342c6bf7a4fdfa8c",
+    "url": "/cvbuilder/static/media/template0.58395202.png"
+  },
+  {
+    "revision": "f0f9e719d95f48dbf34a472eb1bc2f0c",
+    "url": "/cvbuilder/static/media/template1.f0f9e719.png"
+  },
+  {
+    "revision": "1334055d0dbf916d2bc2acf898cb8c01",
+    "url": "/cvbuilder/static/media/template2.1334055d.png"
+  },
+  {
+    "revision": "532d1e40b38b1568cbc5f932267262c4",
+    "url": "/cvbuilder/static/media/template3.532d1e40.png"
+  }
+]);
